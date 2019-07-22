f151c25a4fd385546ded60c955ac93524ced39d8
Oddmund Huseby <huseby.oddmund@gmail.com>
2019-07-22 16:21:08 +0200

    =access-control-allow-origin

--- src/app.js
+++ src/app.js
@@ -9,6 +9,11 @@ const port = process.env.PORT
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next();
+})
 
 module.exports = app
 
